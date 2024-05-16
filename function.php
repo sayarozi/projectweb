2764bebe927a43734554ffb94014d69ad55f842e
sayarozi <achmad_23510017@stimata.ac.id>
2024-05-16 20:15:25 +0700

    project web part 2

--- function.php
+++ function.php
@@ -26,3 +26,23 @@ if (isset($_POST['login'])){
         </script>';
     }
 }
+if(isset($_POST['tambahproduk'])){
+    //deskripsi initial variabel
+    $nama_produk = $_POST['nama_produk'];
+    $deskripsi = $_POST['deskripsi'];
+    $harga = $_POST['harga'];
+    $stock = $_POST['stock'];
+
+    $insert_produk = mysqli_query($koneksi, "INSERT INTO produk (nama_produk, deskripsi, harga, stock) VALUES 
+    ('$nama_produk', '$deskripsi', '$harga', '$stock')");
+
+    if ($insert_produk){
+        header('location:stock.php');
+    }else{
+        echo'
+        <script>
+        alert("gagal tambah produk")
+        window.location.href="stock.php"
+        </script>';
+    }
+}
